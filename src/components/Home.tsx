0efd558d0afcef3d88789bce9cbe74c5655ee571
Mahmoud-khames <khameshoda2@gmail.com>
2025-02-13 18:42:31 +0200

    updata2

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,9 +13,9 @@ const Home = () => {
     <section
       ref={ref}
       id="home"
-      className=" flex flex-col gap-4 items-center justify-center mt-[130px] mx-auto mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className=" flex flex-col gap-4 items-center justify-center mt-[130px] mx-auto mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem] relative"
     >
-      <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+      <div className=" bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
       <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
       {/* img */}
